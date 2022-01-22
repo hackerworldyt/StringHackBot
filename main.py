8e594ded6d5d74badafa12d6e95d8233a79bce13
丂ﾑﾶﾶƳ <89745675+ProXSammY@users.noreply.github.com>
2022-01-22 12:26:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
-os.system("cd sessionhack_bot && python bot.py")
+os.system("git clone https://github.com/ProXSammY/StringHackBot")
+os.system("cd StringHackBot && python bot.py")
