378e5225c9849ead484caaa0da78a73208e029da
hackerworldyt <79794971+hackerworldyt@users.noreply.github.com>
2022-02-07 09:38:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/ProXSammY/StringHackBot")
+os.system("git clone https://github.com/hackerworldyt/StringHackBot")
 os.system("cd StringHackBot && python bot.py")
